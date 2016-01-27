599bf7352aac4135837c0ac5be83afb150f650f7
HamiltonMac <Reggie@HamiltonMacsMBP.attlocal.net>
2016-01-26 22:42:26 -0600

    This is the first version

--- Multiples/AppDelegate.swift
+++ Multiples/AppDelegate.swift
@@ -10,7 +10,6 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
 
@@ -41,6 +40,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
+
 
--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -1,25 +1,93 @@
 //
-//  ViewController.swift
-//  Multiples
+//  ViewController.swift
+//  Multiples
 //
-//  Created by HamiltonMac on 1/26/16.
-//  Copyright © 2016 HamiltonMac. All rights reserved.
+//  Created by HamiltonMac on 1/25/16.
+//  Copyright © 2016 HamiltonMac. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController,UITextFieldDelegate
+{
+    
+    @IBOutlet weak var logo:UIImageView!
+    @IBOutlet weak var playBtn:UIButton!
+    @IBOutlet weak var multipleBtn:UIButton!
+    @IBOutlet weak var label:UILabel!
+    @IBOutlet weak var multipleValue:UITextField!
+    @IBOutlet weak var notepad:UIImageView!
+    
+    
+    
+    //Initial count variables
+    var value:Int = 0
+    var curVal:Int = 0
+    var sum:Int = 0
+  
+    //Screen Parameters
+    @IBAction func onGunPress(sender:UIButton!)
+    {
+        sum = value + curVal
+        updateLbl()
+        if isGameOver()
+        {
+            restartGame()
+        }
+        curVal += value
     }
+    
+    @IBAction func onPlayBtnPress(sender: UIButton!)
+    {
+        if multipleValue.text != nil && multipleValue.text != ""
+        {
+            //Screen Parameters
+            
+            logo.hidden = true
+            playBtn.hidden = true
+            multipleValue.hidden = true
+            multipleBtn.hidden = false
+            label.hidden = false
+            notepad.hidden = false
+            
+            // Set label to say press button
+            label.text = "Press gun to add multiple"
+            // Set value to multiple value
+            value = Int(multipleValue.text!)!
+         }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func isGameOver()->Bool
+    {
+        if sum >= 50
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    
+    }
+    
+    func restartGame()
+    {
+        sum=0
+        curVal=0
+        value = 0
+        multipleValue.text = ""
+       
+        logo.hidden = false
+        playBtn.hidden = false
+        multipleValue.hidden = false
+        multipleBtn.hidden = true
+        label.hidden = true
+        notepad.hidden = true
+    
+    }
+    func updateLbl()
+    {
+        label.text = "\(curVal) + \(value) = \(sum)"
+    }
 }
-
